3add4da9f75ad68a6ca4efc7d58dc79f414b0a6e
Torres <jocta.torres@gmail.com>
2020-11-22 22:17:21 -0300

    add content and repr to Node

--- grafive/model/graph.py
+++ grafive/model/graph.py
@@ -2,7 +2,7 @@ from __future__ import annotations
 
 from contextlib import suppress
 from dataclasses import dataclass, field
-from typing import Any, Set
+from typing import Any, Set, Optional, Dict
 
 from uuid import uuid4 , UUID
 
@@ -12,11 +12,15 @@ from grafive.model.color import Color
 @dataclass
 class Node:
     id: int = field(default_factory= lambda: hash(uuid4()))
-    color: Color = Color.BLACK
+    color: Optional[Color] = None
     connections: Set[Node] = field(default_factory=set)
+    content: Dict[Any, Any] = field(default_factory=dict)
 
     def __hash__(self):
         return self.id
+    
+    def __repr__(self):
+        return f"Node(id={self.id}, color={self.color}, content={self.content})"
     
     @property
     def degree(self):
@@ -34,3 +38,4 @@ class Node:
 class Graph:
     def __init__(self, *nodes):
         self.nodes = set(nodes)
+
