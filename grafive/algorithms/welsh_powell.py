11127275c126d27c03e44d0c928f103c3105fc75
Torres <jocta.torres@gmail.com>
2020-11-29 00:48:10 -0300

    fix welsh_powell

--- grafive/algorithms/welsh_powell.py
+++ grafive/algorithms/welsh_powell.py
@@ -18,9 +18,22 @@ def welsh_powell(graph: Graph):
         color = get_new_color()
         node.color = color
 
-        non_adjacent_nodes = graph.nodes_not_connected_to(node)
-        for other_node in non_adjacent_nodes:
-            other_node.color = color
+        non_adjacent_nodes = sorted(
+            graph.nodes_not_connected_to(node),
+            key=attrgetter("degree"),
+            reverse=True
+        )
+
+        nodes_not_to_color = set()
+        for non_adjacent_node in non_adjacent_nodes:
+            if non_adjacent_node in nodes_not_to_color:
+                continue
+            nodes_not_to_color.update(non_adjacent_node.connections)
+    
+        nodes_to_color = set(non_adjacent_nodes) - nodes_not_to_color
+
+        for node_to_color in nodes_to_color:
+            node_to_color.color = color
 
 
 unused_colors = [
