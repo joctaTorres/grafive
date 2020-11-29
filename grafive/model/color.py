463d3a4c905505e976f5822ede0524d8310333ec
Torres <jocta.torres@gmail.com>
2020-11-29 11:37:49 -0300

    add color generator to color file

--- grafive/model/color.py
+++ grafive/model/color.py
@@ -1,4 +1,5 @@
 from enum import Enum
+import random
 
 class Color(Enum):
     RED = "red"
@@ -6,3 +7,27 @@ class Color(Enum):
     GREEN = "green"
     BLACK = "black"
     PINK = "pink"
+    WHITE = "white"
+    OLIVE = "olive"
+    YELLOW = "yellow"
+    CYAN = "cyan"
+    MAGENTA = "magenta"
+
+
+def iterate_colors() -> Color:
+    for color in list(Color):
+        yield color
+
+def random_unique_color():
+    colors = set()
+    r = lambda: random.randint(0,255)
+
+    while True:
+        color_hex = ('#%02X%02X%02X' % (r(),r(),r())) 
+
+        while color_hex in colors:
+            color_hex = ('#%02X%02X%02X' % (r(),r(),r()))
+        
+        colors.add(color_hex)
+
+        yield color_hex
