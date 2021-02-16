a3b5007232bfb86bcc303744f53c96c24fff0c2e
Joctã Torres <jocta.torres@gmail.com>
2021-02-15 22:43:08 -0300

    change connection factory combination strategy

--- grafive/model/graph.py
+++ grafive/model/graph.py
@@ -1,7 +1,8 @@
 from __future__ import annotations
 
-from typing import Callable, Set
+from typing import Callable, Set, Hashable
 from itertools import combinations
+from collections import defaultdict
 
 from contextlib import suppress
 from dataclasses import dataclass, field
@@ -13,7 +14,7 @@ from grafive.model.color import Color
 
 @dataclass
 class Node:
-    id: int = field(default_factory= lambda: hash(uuid4()))
+    id: int = field(default_factory=lambda: hash(uuid4()))
     color: Optional[Color] = None
     connections: Set[Node] = field(default_factory=set)
     content: Dict[Any, Any] = field(default_factory=dict)
@@ -21,23 +22,27 @@ class Node:
 
     def __hash__(self):
         return self.id
-    
+
     def __eq__(self, other):
         return hash(self) == hash(other)
-    
+
     def __repr__(self):
         return f"Node(id={self.id}, color={self.color}, content={self.content})"
-    
+
     @property
     def degree(self):
         return len(self.connections)
-    
+
     def connect(self, node: Node):
         self.connections.add(node)
         node.connections.add(self)
 
         if self.update_connection_hook:
             self.update_connection_hook(self)
+
+    def connect_all(self, nodes: Set[Node]):
+        for node in nodes:
+            self.connect(node)
 
     def disconnect(self, node: Node):
         self.connections.discard(node)
@@ -47,48 +52,46 @@ class Node:
             self.update_connection_hook(self)
 
 
-
-
 class Graph:
-    def __init__(
-        self,
-        *nodes,
-        connection_factory: Callable[[Node, Node], bool]=None
-    ):
+    def __init__(self, *nodes, connection_factory: Callable[[Node], Hashable] = None):
         self.nodes = set(nodes)
         self.connection_factory = connection_factory
 
         self.connections = {}
         for node in self.nodes:
-            self.connections.update(
-                {node.id: node.connections}
-            )
+            self.connections.update({node.id: node.connections})
             node.update_connection_hook = self.update_connection_hook
 
         if connection_factory:
             self._create_connections()
 
     def _create_connections(self):
-        for node, other_node in combinations(self.nodes, 2):
-            should_connect = self.connection_factory(node, other_node)
-            if should_connect:
-                node.connect(other_node)
-    
-    def update_connection_hook(self, node):
-        self.connections.update(
-            {node.id: node.connections}
-        )
+        connection_groups = defaultdict(set)
 
-            
+        for node in self.nodes:
+            key = self.connection_factory(node)
+            group = connection_groups[key]
+            node.connect_all(group)
+            group.add(node)
+
+    def update_connection_hook(self, node):
+        self.connections.update({node.id: node.connections})
+
     def __repr__(self):
         def get_connection_ids(start_node):
             return "-".join([str(node.id) for node in start_node.connections])
 
-        return "\n".join([f"[{node.id}][{node.color}] :: {get_connection_ids(node)}" for node in self.nodes])
+        return "\n".join(
+            [
+                f"[{node.id}][{node.color}] :: {get_connection_ids(node)}"
+                for node in self.nodes
+            ]
+        )
 
     @property
     def colors(self):
         return {node.color for node in self.nodes if node.color}
+
     @property
     def chromatic_number(self):
         return len(self.colors)
